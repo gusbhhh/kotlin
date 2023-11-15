2636ed86d5a93d3bac5f216d45ee321f35492453
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2023-11-15 14:08:11 +0200

    [FIR2IR] Generate IR for unbound symbols in `allowNonCachedDeclarations` mode
    `allowNonCachedDeclarations` mode allows referring source declaration which
      does not belong to the set of sources passed to fir2ir (e.g. for debugger
      support). So if code refers to such declaration, fir2ir creates symbol
      for it but not the IR declaration itself

--- analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/compilerFacility/FirIdeNormalAnalysisLibrarySourceModuleCompilerFacilityTestGenerated.java
+++ analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/compilerFacility/FirIdeNormalAnalysisLibrarySourceModuleCompilerFacilityTestGenerated.java
@@ -209,6 +209,12 @@ public class FirIdeNormalAnalysisLibrarySourceModuleCompilerFacilityTestGenerate
             }
 
             @Test
+            @TestMetadata("localDelegatedPropery.kt")
+            public void testLocalDelegatedPropery() throws Exception {
+                runTest("analysis/analysis-api/testData/components/compilerFacility/compilation/codeFragments/capturing/localDelegatedPropery.kt");
+            }
+
+            @Test
             @TestMetadata("localFunction.kt")
             public void testLocalFunction() throws Exception {
                 runTest("analysis/analysis-api/testData/components/compilerFacility/compilation/codeFragments/capturing/localFunction.kt");
@@ -260,6 +266,12 @@ public class FirIdeNormalAnalysisLibrarySourceModuleCompilerFacilityTestGenerate
             @TestMetadata("localMutated.kt")
             public void testLocalMutated() throws Exception {
                 runTest("analysis/analysis-api/testData/components/compilerFacility/compilation/codeFragments/capturing/localMutated.kt");
+            }
+
+            @Test
+            @TestMetadata("localVariable.kt")
+            public void testLocalVariable() throws Exception {
+                runTest("analysis/analysis-api/testData/components/compilerFacility/compilation/codeFragments/capturing/localVariable.kt");
             }
 
             @Test
