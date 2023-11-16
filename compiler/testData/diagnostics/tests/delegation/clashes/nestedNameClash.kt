96d886e3888eecd7ba60a3f64521d79528cacda4
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-11-16 15:48:11 +0100

    [FIR] add more tests diagnostic tests on data and delegated classes
    ^KT-63522
    ^KT-63512
    ^KT-63042

---
+++ compiler/testData/diagnostics/tests/delegation/clashes/nestedNameClash.kt
@@ -0,0 +1,17 @@
+// FIR_IDENTICAL
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+// FIR_DUMP
+package second
+
+fun main() {
+    val data = MyClass(object : Base {})
+    data.foo()
+}
+
+interface Base {
+    fun foo() {}
+}
+
+class MyClass(val prop: second.Base): Base by prop {
+    interface Base
+}
---
+++ compiler/testData/diagnostics/tests/delegation/clashes/nestedNameClash2.kt
@@ -0,0 +1,17 @@
+// FIR_IDENTICAL
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+// FIR_DUMP
+package second
+
+fun main() {
+    val data = MyClass(object : Base<Base<Int>> {})
+    data.foo()
+}
+
+interface Base<A> {
+    fun foo() {}
+}
+
+class MyClass(val prop: second.Base<second.Base<Int>>): Base<Base<Int>> by prop {
+    interface Base<B>
+}
