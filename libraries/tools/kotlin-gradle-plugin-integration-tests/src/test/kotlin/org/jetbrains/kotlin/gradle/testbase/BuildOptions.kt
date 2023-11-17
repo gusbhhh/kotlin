8d8dc8e345c472e6e93bb33cec2e457a4efb0f3e
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-11-17 20:23:08 +0100

    fixup! [Gradle] Introduce 'kotlin.internal.compiler.arguments.log.level' property

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
@@ -54,7 +54,7 @@ data class BuildOptions(
     val runViaBuildToolsApi: Boolean? = null,
     val konanDataDir: Path? = konanDir, // null can be used only if you are using custom 'kotlin.native.home' or 'org.jetbrains.kotlin.native.home' property instead of konanDir
     val kotlinUserHome: Path? = testKitDir.resolve(".kotlin"),
-    val compilerArgumentsLogLevel: String = "info"
+    val compilerArgumentsLogLevel: String? = "info"
 ) {
     val isK2ByDefault
         get() = KotlinVersion.DEFAULT >= KotlinVersion.KOTLIN_2_0
@@ -223,7 +223,9 @@ data class BuildOptions(
             arguments.add("-Pkotlin.user.home=${kotlinUserHome.absolutePathString()}")
         }
 
-        arguments.add("-Pkotlin.internal.compiler.arguments.log.level=$compilerArgumentsLogLevel")
+        if (compilerArgumentsLogLevel != null) {
+            arguments.add("-Pkotlin.internal.compiler.arguments.log.level=$compilerArgumentsLogLevel")
+        }
 
         arguments.addAll(freeArgs)
 
